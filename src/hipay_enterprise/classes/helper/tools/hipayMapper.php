24cd5d3930d39178cc36eb1a84fdf129f42f6591
Etienne Landais <elandais@hipay.com>
2017-07-10 12:16:11 +0200

    prestashop validator code styling fixing

--- src/hipay_enterprise/classes/helper/tools/hipayMapper.php
+++ src/hipay_enterprise/classes/helper/tools/hipayMapper.php
@@ -66,7 +66,11 @@ class HipayMapper
     {
         return Carrier::getCarriers(
             $this->context->language->id,
-            true
+            true,
+            false,
+            false,
+            null,
+            Carrier::ALL_CARRIERS
         );
     }
 
