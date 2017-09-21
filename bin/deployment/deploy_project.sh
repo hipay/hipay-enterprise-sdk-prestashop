325547d8bc4fb7affc69a275aa0c6491daf52345
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-21 15:18:13 +0200

    Add packaging

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -6,7 +6,7 @@ echo "Create Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $C
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port mkdir /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
 
 echo "Transfert Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
-sshpass -p $PASS_DEPLOY scp -P $port ./package-ready-for-prestahsop/*.zip root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
+sshpass -p $PASS_DEPLOY scp -P $port ./package-ready-for-prestashop/*.zip root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
 
 echo "Deploy project in artifactory"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
