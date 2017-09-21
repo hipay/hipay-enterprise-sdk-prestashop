fde1755ef9e4ec6901b8c6726cb99515464c8b9d
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-21 16:05:00 +0200

    Add packaging

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -9,7 +9,8 @@ echo "Transfert Artifact project for project $CIRCLE_PROJECT_REPONAME and branch
 sshpass -p $PASS_DEPLOY scp -P $port ./package-ready-for-prestashop/*.zip root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
 
 echo "Deploy project in artifactory"
-sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port jfrog rt u /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH/*.zip "hipay/org.hipay/" \
+sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
+    "jira-artifactory-pi.hipay-pos-platform" chmod u+x ./tmp/jfrog && ./tmp/jfrog rt u /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH/*.zip "hipay/org.hipay/" \
     --build-name=Test --build-number=1 --flat=true --user=admin --password=$ARTIFACTORY_PASSWORD --url http://localhost:8081/artifactory
 
 echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
