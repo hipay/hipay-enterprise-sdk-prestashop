1958804c2b7c6ff119b40884787cc3eba3fb6d89
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-22 09:41:18 +0200

    Add packaging

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -11,7 +11,7 @@ sshpass -p $PASS_DEPLOY scp -P $port ./package-ready-for-prestashop/*.zip root@d
 echo "Deploy project in artifactory"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
     "jira-artifactory-pi.hipay-pos-platform.com" /tmp/jfrog rt u /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH/*.zip $CIRCLE_PROJECT_REPONAME/snapshot/ \
-    --flat=true --user=admin --password=$ARTIFACTORY_PASSWORD --url http://localhost:8081/artifactory/hipay
+    --flat=true --user=admin --password=$ARTIFACTORY_PASSWORD --url http://localhost:8081/artifactory/hipay/
 
 echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
