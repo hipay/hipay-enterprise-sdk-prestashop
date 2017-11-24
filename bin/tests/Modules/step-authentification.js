066909a1ea28177a91ff15be9576377d7e3999a3
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2017-11-24 12:02:10 +0100

    Test

--- bin/tests/Modules/step-authentification.js
+++ bin/tests/Modules/step-authentification.js
@@ -13,14 +13,14 @@ exports.proceed = function proceed(test) {
                 test.info("Connected");
             }, function fail() {
                 test.assertExists(".error-msg", "Incorrect credentials !");
-            }, 30000);
+            }, 40000);
         }, function fail() {
             this.waitForUrl(/controller=/, function success() {
                 test.info("Already logged to admin panel !");
             }, function fail() {
                 test.assertUrlMatch(/controller=/, "Already connected");
             });
-        },20000);
+        },30000);
 
     });
 };
