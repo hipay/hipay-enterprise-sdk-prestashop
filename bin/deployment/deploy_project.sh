90f3e2219855a8088efdfab286c30cd72fdcb566
Berthelot Aymeric <aberthelot@hipay.com>
2018-02-13 11:45:04 +0100

    Test Package

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -4,6 +4,8 @@ echo "Create Artifact project for project $CI_PROJECT_NAME and branch $GITHUB_BR
 docker exec deploy.hipay-pos-platform.com mkdir /deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH
 
 echo "Transfert Artifact project for project $CI_PROJECT_NAME and branch $GITHUB_BRANCH"
+pwd
+ls package-ready-for-prestashop/
 docker cp ./package-ready-for-prestashop/*.zip deploy.hipay-pos-platform.com:/deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH
 
 echo "Deploy project in artifactory"
