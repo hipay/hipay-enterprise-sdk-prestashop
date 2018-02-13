0a3d5019515c1b22c928e77ae8d7cf40eb5a2a66
Berthelot Aymeric <aberthelot@hipay.com>
2018-02-13 11:33:07 +0100

    Test Package

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -1,11 +1,10 @@
 BRANCH=$CI_COMMIT_REF_SLUG
 
 echo "Create Artifact project for project $CI_PROJECT_NAME and branch $GITHUB_BRANCH to /deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH"
-sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org mkdir /deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH
+sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p 32962 mkdir /deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH
 
 echo "Transfert Artifact project for project $CI_PROJECT_NAME and branch $GITHUB_BRANCH"
-sudo chmod 666 /dev/tty
-sshpass -p $PASS_DEPLOY scp ./package-ready-for-prestashop/*.zip root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH
+sshpass -p $PASS_DEPLOY scp -p 32962 ./package-ready-for-prestashop/*.zip root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH
 
 echo "Deploy project in artifactory"
 docker exec jira-artifactory-pi.hipay-pos-platform.com /tmp/jfrog rt u /deploy/project/artifactory/$CI_PROJECT_NAME/$BRANCH/*.zip $CI_PROJECT_NAME/snapshot/ \
