b821035c695c95807da22f174f51700c018f3cca
Berthelot Aymeric <aberthelot@hipay.com>
2018-02-14 15:03:27 +0100

    Fix Circle for public contribution

--- prestashop.sh
+++ prestashop.sh
@@ -45,8 +45,6 @@ if [ "$1" = 'init-production' ] && [ "$2" = '' ];then
 fi
 
 if [ "$1" = 'init-stage' ] && [ "$2" = '' ];then
-     docker-compose -f docker-compose.yml -f docker-compose.stage.yml stop
-     docker-compose -f docker-compose.yml -f docker-compose.stage.yml rm -fv
      docker-compose -f docker-compose.yml -f docker-compose.stage.yml build --no-cache
      docker-compose -f docker-compose.yml -f docker-compose.stage.yml up -d
 fi
