e0e067ab0e33014f40cb651adf31d55f4f3fe2d2
Landais Etienne <elandais@hipay.com>
2018-09-27 09:40:09 +0200

    Prepare release

--- src/hipay_enterprise/hipay_enterprise.php
+++ src/hipay_enterprise/hipay_enterprise.php
@@ -37,7 +37,7 @@ class Hipay_enterprise extends PaymentModule
 
         $this->name = 'hipay_enterprise';
         $this->tab = 'payments_gateways';
-        $this->version = '2.2.6';
+        $this->version = '2.2.7';
         $this->module_key = 'c3c030302335d08603e8669a5210c744';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->currencies = true;
