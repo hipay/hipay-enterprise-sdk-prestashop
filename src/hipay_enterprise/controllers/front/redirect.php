cdcb4f85b98779e41fd4fa741cf22d885a47732f
Landais Etienne <elandais@hipay.com>
2018-10-15 16:33:19 +0200

    Change hosted fields design

--- src/hipay_enterprise/controllers/front/redirect.php
+++ src/hipay_enterprise/controllers/front/redirect.php
@@ -289,7 +289,7 @@ class Hipay_enterpriseRedirectModuleFrontController extends ModuleFrontControlle
         $this->addCSS(array(_MODULE_DIR_ . 'hipay_enterprise/views/css/hipay-enterprise.css'));
         $this->context->controller->addJS(
             array(
-                'https://pi-hostedfields-develop.hipay-pos-platform.com/js/sdkjs.js'
+                'http://stage-libs.hipay.com/js/sdkjs.js'
             )
         );
 
