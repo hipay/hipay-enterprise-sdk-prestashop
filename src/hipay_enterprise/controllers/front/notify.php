b75c977ba408bb89f568be75dfcf4f32b066014b
David ARCENT <darcent.ext@hipay.com>
2023-02-14 15:54:27 +0000

    Resolve EC-2865 "Feature/"

--- src/hipay_enterprise/controllers/front/notify.php
+++ src/hipay_enterprise/controllers/front/notify.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * HiPay Enterprise SDK Prestashop
+ * HiPay Enterprise SDK Prestashop.
  *
  * 2017 HiPay
  *
@@ -10,78 +10,76 @@
  * @copyright 2017 HiPay
  * @license   https://github.com/hipay/hipay-enterprise-sdk-prestashop/blob/master/LICENSE.md
  */
-
-require_once(dirname(__FILE__) . '/../../classes/exceptions/NotificationException.php');
-require_once(dirname(__FILE__) . '/../../classes/helper/HipayNotification.php');
-require_once(dirname(__FILE__) . '/../../classes/helper/HipayHelper.php');
+require_once dirname(__FILE__).'/../../classes/exceptions/NotificationException.php';
+require_once dirname(__FILE__).'/../../classes/helper/HipayNotification.php';
+require_once dirname(__FILE__).'/../../classes/helper/HipayHelper.php';
 
 use HiPay\Fullservice\Enum\Transaction\ECI;
 
 /**
- * Class Hipay_enterpriseNotifyModuleFrontController
+ * Class Hipay_enterpriseNotifyModuleFrontController.
  *
  * @author      HiPay <support.tpp@hipay.com>
  * @copyright   Copyright (c) 2017 - HiPay
  * @license     https://github.com/hipay/hipay-enterprise-sdk-prestashop/blob/master/LICENSE.md
- * @link    https://github.com/hipay/hipay-enterprise-sdk-prestashop
+ *
+ * @see    https://github.com/hipay/hipay-enterprise-sdk-prestashop
  */
 class Hipay_enterpriseNotifyModuleFrontController extends ModuleFrontController
 {
+    /** @var Hipay_entreprise */
+    public $module;
 
     /**
      * @see FrontController::postProcess()
      */
     public function postProcess()
     {
-
-        $this->name = 'notify';
-        if ($this->module->active == false) {
+        if (false == $this->module->active) {
             $this->module->getLogs()->logErrors('Notify : postProcess => Module Disable');
             header('HTTP/1.1 403 Forbidden');
-            die;
+            exit;
         }
 
         try {
             $params = $_POST;
-            $transactionReference = (isset($params["transaction_reference"])) ? $params["transaction_reference"] : '';
+            $transactionReference = (isset($params['transaction_reference'])) ? $params['transaction_reference'] : '';
             // Process log from notification
             $this->module->getLogs()->logCallback($params);
 
             // Check if status is present in Post Data
             if (!isset($params['state']) && !isset($params['status'])) {
                 $this->module->getLogs()->logErrors('Notify : Status not exist in Post DATA');
-                header("HTTP/1.0 500 Internal server error");
-                die();
+                header('HTTP/1.0 500 Internal server error');
+                exit;
             }
 
             // Check Notification signature
-            $signature = (isset($_SERVER["HTTP_X_ALLOPASS_SIGNATURE"])) ? $_SERVER["HTTP_X_ALLOPASS_SIGNATURE"] : "";
-
             $notificationHandler = new HipayNotification($this->module, $params);
 
             $moto = false;
             $isApplePay = false;
-            if ($notificationHandler->getEci() == ECI::MOTO) {
+            if (ECI::MOTO == $notificationHandler->getEci()) {
                 $moto = true;
             } elseif ($notificationHandler->isApplePayOrder()) {
                 $isApplePay = true;
             }
 
-            if (!HipayHelper::checkSignature($signature, $this->module, true, $moto, $isApplePay)) {
+            if (!HipayHelper::checkSignature($this->module, $moto, $isApplePay)) {
                 $this->module->getLogs()->logErrors("Notify : Signature is wrong for Transaction $transactionReference.");
                 header('HTTP/1.1 403 Forbidden');
-                die('Bad Callback initiated - signature');
+                exit('Bad Callback initiated - signature');
             }
 
             $notificationHandler->processTransaction();
         } catch (NotificationException $e) {
             header($e->getReturnCode());
-            die($e->getMessage());
+            exit($e->getMessage());
         } catch (Exception $e) {
-            header("HTTP/1.0 500 Internal server error");
-            die($e->getMessage());
+            header('HTTP/1.0 500 Internal server error');
+            exit($e->getMessage());
         }
 
-        die();
+        exit;
     }
 }
